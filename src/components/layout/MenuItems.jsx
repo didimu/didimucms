90a4da8478b3984596ab1c2cf86d9f868f8381bc
Kim youngOk <kyo2685@didimu.co.kr>
2024-08-20 10:13:37 +0900

    first commit

--- src/components/layout/MenuItems.jsx
+++ src/components/layout/MenuItems.jsx
@@ -1,3 +1,4 @@
+
 import Dropdown from "./Dropdown";
 
  
---
+++ src/pages/UnsplashPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnsplashPage = () => {
+  return (
+    <div>UnsplashPage</div>
+  )
+}
+
+export default UnsplashPage
